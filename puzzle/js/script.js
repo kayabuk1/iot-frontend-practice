ab92fe9c33b92ac8b55b50e570f04913d41025dd
yksk96 <yokosuka0609@gmail.com>
2026-06-04 16:18:47 +0900

    Add files via upload

---
+++ puzzle/js/script.js
@@ -0,0 +1,29 @@
+// alert("HelloWorld!!");
+// alert関数が実行されるとブラウザに警告があがる。
+// 引数を入れておくと、それが表示される。
+console.log("Hello World!!")
+// F12デベロッパーツールを開いてcosoleタブを開くと文字が表示される。
+// ↑の関数は良くデバッグに使うとのこと。
+// console画面の右端に script.js:4 とあるが、どのプログラムで実行されたかが
+// 表示されている。:4は4行目ということ。 
+const setupArea = document.querySelector('#setup-area');
+console.log(setupArea);
+// JSで変数を宣言するときは可能な限り const で宣言した方が良いとのこと。
+// constで宣言してもオブジェクトのプロパティは変更できるとのこと。
+// const imageSelect
+// const imageFile
+// const imageFileLabel
+// const gridSelect
+// const startBtn
+// const gameArea
+// const moveCounter
+// const timer
+// const stautsLbl
+// const puzzleBoard
+// const originalPreview
+// const previewBtn
+// const backBtn
+// const clearArea
+// const finalTime
+// const finalMoves
+// const restartBtn
