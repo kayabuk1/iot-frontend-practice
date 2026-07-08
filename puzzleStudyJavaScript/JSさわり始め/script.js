9056b50cdbaad9d4a211f3e2c3b78be9ff222157
yksk96 <yokosuka0609@gmail.com>
2026-07-08 17:07:11 +0900

    Add files via upload

---
+++ puzzleStudyJavaScript/JSさわり始め/script.js
@@ -0,0 +1,9 @@
+function createParagraph(){
+    const para = document.createElement("p");
+    para.textContent = "ボタンがおされました！";
+    document.body.appendChild(para);
+        }
+const buttons = document.querySelectorAll("button");
+for (const button of buttons){
+    button.addEventListener("click", createParagraph);
+}
